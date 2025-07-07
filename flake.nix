9abc9f371f080e9883609b58226d7c2d57102066
Ethan Thoma <ethanthoma@gmail.com>
2025-07-07 11:53:19 -0700

    tidy: refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -3,67 +3,80 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
     nix-gleam.url = "github:arnarg/nix-gleam";
     nix-gleam-burrito.url = "github:ethanthoma/nix-gleam-burrito";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      nix-gleam,
-      nix-gleam-burrito,
-    }:
-    {
-      overlays = {
-        default = nixpkgs.lib.composeManyExtensions [
-          nix-gleam.overlays.default
-          nix-gleam-burrito.overlays.default
-        ];
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
+    inputs@{ ... }:
 
-        erlangPackage = pkgs.erlang_27;
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.flake-parts.flakeModules.modules
+      ];
 
-        default = pkgs.buildGleamApplication {
-          src = ./.;
-          inherit erlangPackage;
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system}.extend (
+            inputs.nixpkgs.lib.composeManyExtensions [
+              inputs.nix-gleam.overlays.default
+              inputs.nix-gleam-burrito.overlays.default
+            ]
+          );
+        in
+        {
+          _module.args.pkgs = pkgs;
+
+          apps = {
+            default = pkgs.buildGleamApplication {
+              src = ./.;
+              erlangPackage = pkgs.erlang_27;
+            };
+
+            release = pkgs.buildGleamBurrito {
+              src = ./.;
+              erlangPackage = pkgs.erlang_27;
+            };
+          };
+
+          devshells.default = {
+            env = [
+              {
+                name = "MIX_ENV";
+                value = "prod";
+              }
+            ];
+
+            packages = [
+              pkgs.erlang_27
+              pkgs.elixir
+              pkgs.zig
+              pkgs.p7zip
+              pkgs.mix2nix
+              pkgs.gemini-cli
+            ];
+
+            commands = [ { package = pkgs.gleam; } ];
+          };
         };
-
-        release = pkgs.buildGleamBurrito {
-          src = ./.;
-          inherit erlangPackage;
-        };
-
-        build-deps = [
-          pkgs.elixir
-          pkgs.zig
-          pkgs.p7zip
-        ];
-      in
-      {
-        checks = {
-          inherit default;
-        };
-
-        packages = {
-          inherit default;
-          inherit release;
-        };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ default ];
-
-          packages = build-deps ++ [ pkgs.mix2nix ];
-
-          env.MIX_ENV = "prod";
-        };
-      }
-    );
+    };
 }
